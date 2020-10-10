0f1aa6e893a1b84e42d48ad3fe258e95d09d54c8
HimaliWijekoon97 <72617242+HimaliWijekoon97@users.noreply.github.com>
2020-10-10 21:17:19 +0530

    Update NIP.py

--- NIP.py
+++ NIP.py
@@ -7,15 +7,13 @@
 # Python3 program for implementing  
 # Newton divided difference formula  
   
-# Function to find the product term  
 def proterm(i, value, x):  
     pro = 1;  
     for j in range(i):  
         pro = pro * (value - x[j]);  
     return pro;  
   
-# Function for calculating  
-# divided difference table  
+
 def dividedDiffTable(x, y, n): 
   
     for i in range(1, n):  
@@ -24,8 +22,7 @@ def dividedDiffTable(x, y, n):
                                      (x[j] - x[i + j])); 
     return y; 
   
-# Function for applying Newton's  
-# divided difference formula  
+  
 def applyFormula(value, x, y, n):  
   
     sum = y[0][0];  
@@ -35,8 +32,7 @@ def applyFormula(value, x, y, n):
       
     return sum;  
   
-# Function for displaying divided  
-# difference table  
+  
 def printDiffTable(y, n):  
   
     for i in range(n):  
@@ -46,33 +42,7 @@ def printDiffTable(y, n):
   
         print("");  
   
-# Driver Code 
-  
-# number of inputs given  
-n = 4;  
-y = [[0 for i in range(10)]  
-        for j in range(10)];  
-x = [ 5, 6, 9, 11 ];  
-  
-# y[][] is used for divided difference  
-# table where y[][0] is used for input  
-y[0][0] = 12;  
-y[1][0] = 13;  
-y[2][0] = 14;  
-y[3][0] = 16;  
-  
-# calculating divided difference table  
-y=dividedDiffTable(x, y, n);  
-  
-# displaying divided difference table  
-printDiffTable(y, n);  
-  
-# value to be interpolated  
-value = 7;  
-  
-# printing the value  
-print("\nValue at", value, "is", 
-        round(applyFormula(value, x, y, n), 2)) 
+
   
 # This code is contributed by mits 
 
